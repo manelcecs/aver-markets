28dce7a869a4402a675a4aec427af3903dad0ae5
manpergut <manpergut@alum.us.es>
2022-10-26 20:15:15 +0200

    consume and represent aver market API endpoint

--- src/containers/Markets.jsx
+++ src/containers/Markets.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getActiveEvents } from "../services/markets.service";
 
 const Markets = () => {
+  const [markets, setMarkets] = useState([]);
+
+  useEffect(async () => {
+    const response = await getActiveEvents();
+    
+    setMarkets(response.data.results);
+  }, []);
+
   return (
     <div className="markets-container">
       <h2 className="text-gradient animated text-left">Available markets</h2>
+      {markets.map((market) => {
+        return <p>{market.name}</p>;
+      })}
     </div>
   );
 };
