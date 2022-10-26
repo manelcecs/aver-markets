4c7d7f35d32be2b79417df3998b1180b03bf07c4
manpergut <manpergut@alum.us.es>
2022-10-26 21:12:55 +0200

    add market representation and status

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import Market from "./Market";
 
-const Event = ({event}) => {   
-    return ( <p>{event.name}</p> );
-}
- 
+const Event = ({ event }) => {
+  const markets = event.markets;
+  return (
+    <div className="market-container">
+      <h3>{event.name}</h3>
+      {markets.map((market) => {
+        return <Market market={market} key={market.pubkey} />;
+      })}
+    </div>
+  );
+};
+
 export default Event;
--- src/containers/Events.jsx
+++ src/containers/Events.jsx
@@ -4,13 +4,12 @@ import useGetEvents from "../hooks/useGetEvents";
 
 const Events = () => {
   // call the custom hook to retrieve events information
-  const markets = useGetEvents();
+  const events = useGetEvents();
 
   return (
     <div className="events-container">
       <h2 className="text-gradient animated text-left">Available events</h2>
-      {markets.map((event) => {
-        debugger;
+      {events.map((event) => {
         // each event will generate a event component that receive the event object.
         // each event component will have the DOM key property with the event id.
         return <Event event={event} key={event.id} />;
